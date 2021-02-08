c9ba579bc78741b0ab62f3fe6b217c0bb6bd2e3e
Alexandru Ica <alexandruica703@gmail.com>
2021-02-08 19:14:44 +0200

    Sample demo

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+#include <glm/glm.hpp>
+
+auto main() -> int
+{
+    constexpr int image_width = 384;
+    constexpr int image_height = 256;
+
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    for(int j = image_height - 1; j >= 0; --j) {
+        for(int i = 0; i < image_width; ++i) {
+            auto const r = double(i) / (image_width - 1);
+            auto const g = double(j) / (image_height - 1);
+            auto const b = 0.25;
+
+            constexpr double max_color = 255.999;
+
+            std::cout << static_cast<int>(max_color * r) << ' ';
+            std::cout << static_cast<int>(max_color * g) << ' ';
+            std::cout << static_cast<int>(max_color * b) << '\n';
+        }
+    }
+}
