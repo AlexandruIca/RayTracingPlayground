37b9c8cbb40b2ca477572e1d53dd117f6a8d36e8
Alexandru Ica <alexandruica703@gmail.com>
2021-02-11 13:24:13 +0200

    Add `random_double` and `write_color_over_samples` functions
    - `random_double` returns a random number between 0.0 and 1.0, there is
    also `random_double_between(min, max)` that will return a random double between
    min and max
    - `write_color_over_samples` that will take the average color of
    multiple samples

--- src/utilities.cpp
+++ src/utilities.cpp
@@ -1,6 +1,8 @@
 #include "utilities.hpp"
 
+#include <algorithm>
 #include <iostream>
+#include <random>
 
 auto write_color(std::ostream& os, color const& pixel_color) -> void
 {
@@ -9,4 +11,37 @@ auto write_color(std::ostream& os, color const& pixel_color) -> void
     os << static_cast<int>(max_color * pixel_color.r) << ' ';
     os << static_cast<int>(max_color * pixel_color.g) << ' ';
     os << static_cast<int>(max_color * pixel_color.b) << '\n';
+}
+
+auto write_color_over_samples(std::ostream& os, color const& pixel_color, int const spp) -> void
+{
+    constexpr double max_factor = 0.999;
+    constexpr double max_color = 256.0;
+
+    auto r = pixel_color.r;
+    auto g = pixel_color.g;
+    auto b = pixel_color.b;
+
+    auto const scale = 1.0 / double(spp);
+
+    r *= scale;
+    g *= scale;
+    b *= scale;
+
+    os << static_cast<int>(max_color * std::clamp(r, 0.0, max_factor)) << ' '
+       << static_cast<int>(max_color * std::clamp(g, 0.0, max_factor)) << ' '
+       << static_cast<int>(max_color * std::clamp(b, 0.0, max_factor)) << '\n';
+}
+
+auto random_double() -> double
+{
+    static std::mt19937 rng{ std::random_device{}() };
+    static std::uniform_real_distribution<double> dist{ 0.0, 1.0 };
+    return dist(rng);
+}
+
+auto random_double_between(double const min, double const max) -> double
+{
+    // developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random#getting_a_random_number_between_two_values
+    return min + (max - min) * random_double();
 }
--- src/utilities.hpp
+++ src/utilities.hpp
@@ -11,5 +11,20 @@ using point3 = vec3;
 using color = glm::dvec3;
 
 auto write_color(std::ostream& os, color const& pixel_color) -> void;
+///
+/// \param spp How many samples per pixel
+///
+/// This function will do the same thing as \ref write_color but will divide the color by `spp`.
+///
+auto write_color_over_samples(std::ostream& os, color const& pixel_color, int spp) -> void;
+
+///
+/// \returns A random number between 0.0 and 1.0
+///
+auto random_double() -> double;
+///
+/// \returns A random number between min and max
+///
+auto random_double_between(double min, double max) -> double;
 
 #endif // !UTILITIES_HPP
