950edde5ceb37e1855b6f206a6eefeb00b937740
Alexandru Ica <alexandruica703@gmail.com>
2021-02-11 16:40:58 +0200

    Implement a (non gamma corrected) diffuse material

--- tests/diffuse_material.cpp
+++ tests/diffuse_material.cpp
@@ -30,10 +30,10 @@
 auto main() -> int
 {
     constexpr double aspect_ratio = 16.0 / 9.0;
-    constexpr int image_width = 1400;
+    constexpr int image_width = 350;
     constexpr int image_height = static_cast<int>(static_cast<double>(image_width) / aspect_ratio);
     constexpr int samples_per_pixel = 10;
-    constexpr int max_depth = 10;
+    constexpr int max_depth = 5;
 
     camera cam{};
     hittable_list world{};
