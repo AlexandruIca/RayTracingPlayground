437614d8d1cf0765373c2f80d10f3b2e98890f36
Alexandru Ica <alexandruica703@gmail.com>
2021-02-11 13:27:51 +0200

    Test the new camera class, taking multiple samples for each ray

--- tests/antialiasing.cpp
+++ tests/antialiasing.cpp
@@ -24,9 +24,9 @@
 auto main() -> int
 {
     constexpr double aspect_ratio = 16.0 / 9.0;
-    constexpr int image_width = 1400;
+    constexpr int image_width = 560;
     constexpr int image_height = static_cast<int>(static_cast<double>(image_width) / aspect_ratio);
-    constexpr int samples_per_pixel = 100;
+    constexpr int samples_per_pixel = 25;
 
     camera cam{};
     hittable_list world{};
